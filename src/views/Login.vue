d81910ea0631d96c31779a110b8d3999edec0c00
Tushar <tusharbedi7373@hotmail.com>
2018-07-20 14:30:43 +0700

    added a working UI for Logging In

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,14 @@
 <template>
 <div>
-    <RegisterForm> </RegisterForm>
     <LoginForm> </LoginForm>
 </div>
 </template>
 
 <script>
 //import component first
-import RegisterForm from '@/components/RegisterForm.vue'
 import LoginForm from '@/components/LoginForm.vue'
 export default {
     components: {
-        RegisterForm,
         LoginForm
     }
 }
