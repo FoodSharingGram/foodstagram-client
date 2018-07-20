db699f36143b665a1c607ca58ea8a9cf103de18f
Tushar <tusharbedi7373@hotmail.com>
2018-07-20 14:22:09 +0700

    Haandled Error Messages

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -7,11 +7,9 @@
 <script>
 //import component first
 import RegisterForm from '@/components/RegisterForm.vue'
-import LoginForm from '@/components/LoginForm.vue'
 export default {
     components: {
-        RegisterForm,
-        LoginForm
+        RegisterForm
     }
 }
 </script>
